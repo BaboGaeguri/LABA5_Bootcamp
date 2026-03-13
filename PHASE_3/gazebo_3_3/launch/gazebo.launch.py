6c0e472eacda838b4947c458b51f8ffa7434ee5b
Babogaeguri <psyun0323@gmail.com>
2026-03-13 21:53:32 +0900

    update

---
+++ PHASE_3/gazebo_3_3/launch/gazebo.launch.py
@@ -0,0 +1,48 @@
+import os
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    urdf_file = os.path.join(
+        get_package_share_directory('gazebo_3_3'),
+        'urdf',
+        'wheeled_robot.urdf'
+    )
+
+    robot_description = ParameterValue(
+        Command(['cat ', urdf_file]),
+        value_type=str
+    )
+
+    return LaunchDescription([
+
+        # 1. Gazebo 실행
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+
+        # 2. robot_state_publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+            output='screen'
+        ),
+
+        # 3. Gazebo에 로봇 소환
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-topic', 'robot_description', '-entity', 'wheeled_robot'],
+            output='screen'
+        ),
+
+    ])
