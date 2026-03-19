60a6954ea8b4a133445e44c83ba929f1955274e4
winningsushi <shkim9808@naver.com>
2026-03-20 05:33:29 +0900

    update

--- PHASE_1/hardware_test.py
+++ PHASE_1/hardware_test.py
@@ -1,3 +1,4 @@
+'''
 import sys
 import tty
 import termios
@@ -100,3 +101,15 @@ finally:
     GPIO.cleanup()
     servo.detach()
     print("\n종료.")
+'''
+
+from gpiozero import AngularServo
+import time
+
+servo = AngularServo(18, min_pulse_width=0.0005, max_pulse_width=0.0025)
+
+while True:
+    servo.angle = 0
+    time.sleep(1)
+    servo.angle = 180
+    time.sleep(1)
