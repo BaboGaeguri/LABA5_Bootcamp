21f6262b5b45c219ec2195d8393e1681941660f5
winningsushi <shkim9808@naver.com>
2026-03-20 04:29:01 +0900

    update

--- PHASE_1/hardware_test.py
+++ PHASE_1/hardware_test.py
@@ -18,8 +18,8 @@ servo = AngularServo(
     18,
     min_angle=0,
     max_angle=180,
-    min_pulse_width=0.001,
-    max_pulse_width=0.002
+    min_pulse_width=0.0005,
+    max_pulse_width=0.0025
 )
 
 current_angle = 90
