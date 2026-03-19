445892a74e44525f83b5e955633a386a6e4b7be4
winningsushi <shkim9808@naver.com>
2026-03-20 05:36:19 +0900

    update

--- PHASE_1/hardware_test.py
+++ PHASE_1/hardware_test.py
@@ -106,10 +106,10 @@ finally:
 from gpiozero import AngularServo
 import time
 
-servo = AngularServo(18, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo = AngularServo(18, min_angle=-90, max_angle=90, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 while True:
-    servo.angle = 0
+    servo.angle = -90
     time.sleep(1)
-    servo.angle = 180
+    servo.angle = 90
     time.sleep(1)
